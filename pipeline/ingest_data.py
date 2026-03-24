7c8a53ce7921733cf9fa0b32f12db027d89c2dcc
ruchi <ruchisppandey@gmail.com>
2026-03-24 11:19:10 +0530

    start data ingestion

---
+++ pipeline/ingest_data.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import pandas as pd
+prefix='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+df = pd.read_csv(prefix + 'yellow_tripdata_2021-01.csv.gz', nrows=100)
+
+df.head()
+df.dtypes
+df.shape
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -10,3 +10,14 @@ df = pd.DataFrame({"A": [1,2], "B": [3, 4]})
 print(df.head())
 
 df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+"""
+docker run -d ^
+  --name postgres-nytaxi ^
+  -e POSTGRES_USER=root ^
+  -e POSTGRES_PASSWORD=root ^
+  -e POSTGRES_DB=ny_taxi ^
+  -e TZ=Asia/Kolkata ^
+  -p 5432:5432 ^
+  postgres:18
+"""
